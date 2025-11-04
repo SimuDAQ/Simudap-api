fcfe2e6cacbaefec717dd193fca035f21369ad2d
0AndWild <parkky3563@gmail.com>
2025-11-04 18:54:06 +0900

    feat: add initial project setting

---
+++ src/main/java/com/simudap/SimudaqApiApplication.java
@@ -0,0 +1,13 @@
+package com.simudap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimudaqApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimudaqApiApplication.class, args);
+    }
+
+}
