ad6271731637cdd27f559e82e3060604389bd980
0AndWild <parkky3563@gmail.com>
2025-11-14 22:25:07 +0900

    fix : fix ObjectMapper jackson data binding with record issue

--- src/main/java/com/simudap/dto/kis_websocket/ClientMessage.java
+++ src/main/java/com/simudap/dto/kis_websocket/ClientMessage.java
@@ -1,12 +1,15 @@
 package com.simudap.dto.kis_websocket;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.simudap.enums.WebsocketAction;
 
 public record ClientMessage(
         WebsocketAction action,
         String stockCode
 ) {
-    public ClientMessage(String action, String stockCode) {
-        this(WebsocketAction.from(action), stockCode);
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public static ClientMessage of(@JsonProperty("action") String action, @JsonProperty("stockCode") String stockCode) {
+        return new ClientMessage(WebsocketAction.from(action), stockCode);
     }
 }
