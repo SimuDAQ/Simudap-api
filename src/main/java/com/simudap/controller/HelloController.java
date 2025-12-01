c25ff43ba9164ee91768e9ed6f1745eb19875eb7
0AndWild <parkky3563@gmail.com>
2025-12-01 23:18:29 +0900

    refactor: clean test controller method

--- src/main/java/com/simudap/controller/HelloController.java
+++ src/main/java/com/simudap/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.simudap.controller;
 
-import com.simudap.facade.StockInformationFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class HelloController {
 
-    private final StockInformationFacade stockInformationFacade;
-
     @GetMapping("/hello")
     public String hello() {
         return "Hello";
-    }
-
-    @GetMapping("/download")
-    public void testDownload() {
-        stockInformationFacade.updateStockInformation();
     }
 }
