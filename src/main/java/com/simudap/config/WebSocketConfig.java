953f52be6b0f705b8ab55f91301cd554d290f7a8
gunyoung <parkky3563@gmail.com>
2026-01-07 15:41:51 +0900

    refactor: change allowOrigins to allowOriginPatterns

--- src/main/java/com/simudap/config/WebSocketConfig.java
+++ src/main/java/com/simudap/config/WebSocketConfig.java
@@ -30,7 +30,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stock")
-                .setAllowedOrigins("*");
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
